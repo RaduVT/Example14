9121d4844ffbdbc2777e929e030f5c54598c6b45
alu10722411 <alutoa2411@ieselcaminas.org>
2022-09-22 16:53:26 +0200

    second commit

--- src/Example14.java
+++ src/Example14.java
@@ -11,32 +11,25 @@ class Example14 {
 
         int y;
         System.out.println("Enter second number:");
-
-        inputValue = new Scanner(System.in);
+        
         y = inputValue.nextInt();
 
         int z;
         System.out.println("Enter third number");
 
-        inputValue = new Scanner(System.in);
         z = inputValue.nextInt();
 
-        if (x >= y && y >= z) {
+        if (x <= y && y <= z) {
             System.out.println("Order is: " + x + " , " + y + " and " + z);
-        }
-        if (x >= z && z >= y) {
+        } else if (x <= z && z <= y) {
             System.out.println("Order is: " + x + " , " + z + " and " + y);
-        }
-        if (y >= x && x >= z) {
+        } else if (y <= x && x <= z) {
             System.out.println("Order is: " + y + " , " + x + " and " + z);
-        }
-        if (y >= z && z >= x) {
+        } else if (y <= z && z <= x) {
             System.out.println("Order is: " + y + " , " + z + " and " + x);
-        }
-        if (z >= x && x >= y) {
+        } else if (z <= x && x <= y) {
             System.out.println("Order is: " + z + " , " + x + " and " + y);
-        }
-        if (z >= y && x >= x) {
+        } else {
             System.out.println("Order is: " + z + " , " + y + " and " + x);
         }
     }
